ef024b2240be6745808e9dc5ec482977f3b58dd4
HeoGwangYoung <heogwangyoung@gmail.com>
2023-04-12 14:49:27 +0900

    First_Program

--- Hello/src/com/callor/hello/HelloGwangju.java
+++ Hello/src/com/callor/hello/HelloGwangju.java
@@ -1,7 +1,8 @@
-package hello;
+package com.callor.hello;
 
-public class Hello {
+public class HelloGwangju {
 	public static void main(String[] args) {
 		System.out.println("Hello Korea");
+		System.out.println(30 + 40);
 	}
 }
---
+++ Hello/src/com/callor/hello/HelloKorea.java
@@ -0,0 +1,18 @@
+package com.callor.hello;
+
+// this class file
+/*Java 의 클래스 파일 만들기
+ *Java 의 클래스 파일은 Source Code File이라고 한다. 
+ * 클래스명.java라는 이름으로 파일이 생성된다
+ * 여기에서는 HelloKorea.java라는 이름으로 파일이 생성
+ * 
+ * "클래스 명은" 가급적 2개 이상의 단어를 조합하여 사용
+ * "클래스명은" 첫글자는 반드시 대문자를 사용한다
+ * 이후 부터는 영문대소문자, 숫자, 언더스코어(_)를 사용할수 있다.
+ * 첫글자를 언더스코어로 사용하기도 하지만
+ * 최근 패턴은 권장하지 않는다
+ * 
+ */
+public class HelloKorea { // block 은 blade(중괄호)로 시작
+
+}// end HelloKorea block, blade 는 반드시 쌍을 이루어야 한다.
